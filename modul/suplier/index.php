1c303b69d925ef6c3390c51dbfc8b8b760b7eaba
laurentinaLuisa <louisasembiring4@gmail.com>
2024-06-14 20:18:10 +0700

    update

--- modul/suplier/index.php
+++ modul/suplier/index.php
@@ -1,129 +1,53 @@
-<div class="card mb-3">
-<div class="card-body">
-<div class="row">
-<div class="mb-3 col-md-6">
-<label for="nama_suplier" class="form-label">Nama suplier</label>
-<input type="text" class="form-control" name="nama_suplier">
-</div>
-<div class="mb-3 col-md-6">
-<label for="alamat" class="form-label">Alamat</label>
-<input type="text" class="form-control" name="alamat">
-</div>
-</row>
-<div class="row">
-<div class="mb-3 col-md-6">
-<label for="telp" class="form-label">Telp</label>
-<input type="text" class="form-control" name="telp">
-</div>
-<div class="mb-3 col-md-6">
-<label for="email" class="form-label">Email</label>
-<input type="text" class="form-control" name="email">
-</div>
-</div>
-<hr>
-<div class="row">
-<div class="col text-end">
-<button class="btn btn-secondary" type="reset">Reset</button>
-<button class="btn btn-primary" type="submit">Simpan</button>
-</div>
-</div>
-</div>
-</div>
-<div class="card">
-<div class="card-header">
-<h3>Data Suplier</h3>
-</div>
-<div class="card-body">
-<div class="table-responsive">
-<table class="table table-striped">
-<thead>
-<tr>
-<th>#</th>
-<th>Nama Suplier</th>
-<th>Alamat</th>
-<th>Telp</th>
-<th>Email</th>
-<th><i class="bi bi-gear-fill"></i></th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>1</td>
-<td>PT. ABC</td>
-<td>Jl. ABC</td>
-<td>08123456789</td>
-<td>oK5Zt@example.com</td>
-<td>
-<a href="#editSuplier" class="text-decoration-none" data-bs-
-toggle="modal">
-<i class="bi bi-pencil-square text-success"></i>
-</a>
-<a href="" class="text-decoration-none">
-<i class="bi bi-trash text-danger"></i>
-</a>
-</td>
-<!-- Modal -->
-<div class="modal fade" id="editSuplier" tabindex="-1" aria-
-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-<div class="modal-content">
-<div class="modal-header">
-Suplier</h5>
-<h5 class="modal-title" id="exampleModalLabel">Edit
-<button type="button" class="btn-close" data-bs-
-dismiss="modal" aria-label="Close"></button>
-</div>
-<div class="modal-body">
-<div class="mb-3">
-<label class="form-label" for="nama_suplier">Nama
-suplier</label>
-<input type="text" class="form-control"
-name="nama_suplier" value="PT. ABC">
-</div>
-<div class="mb-3">
-<label class="form-label" for="alamat">Alamat</label>
-value="Jl. ABC">
-<input type="text" class="form-control" name="alamat"
-</div>
-<div class="mb-3">
-<label class="form-label" for="telp">Telp</label>
-<input type="text" class="form-control" name="telp"
-value="08123456789">
-</div>
-<div class="mb-3">
-<label class="form-label" for="email">Email</label>
-<input type="text" class="form-control" name="email"
-value="oK5Zt@example.com">
-</div>
-</div>
-<div class="modal-footer">
-<button type="button" class="btn btn-secondary" data-bs-
-dismiss="modal">Close</button>
-<button type="button" class="btn btn-primary">Save
-changes</button>
-</div>
-</div>
-</div>
-</div>
-</tr>
-<tr>
-<td>2</td>
-<td>PT. XYZ</td>
-<td>J1. XYZ</td>
-<td>08123456789</td>
-<td>oK5Zt@example.com</td>
-<td>
-<a href="#editSuplier" class="text-decoration-none"
-data-bs-toggle="modal">
-<i class="bi bi-pencil-square text-success"></i>
-</a>
-<a href="" class="text-decoration-none">
-<i class="bi bi-trash text-danger"></i>
-</a>
-</td>
-</tr>
-</tbody>
-</table>
-</div>
-</div>
-</div>
+<?php
+session_start();
+include_once('../../koneksi.php');
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $nama_supplier = $_POST['nama_supplier'];
+    $alamat = $_POST['alamat'];
+    $telepon = $_POST['telepon'];
+    $email = $_POST['email'];
+    if($_GET['act']=="insert"){
+        $query = "INSERT INTO supplier (nama_supplier, alamat, telepon, email) 
+        VALUES ('$nama_supplier','$alamat','$telepon','$email')";
+        $exec = mysqli_query($koneksi, $query);
+        if($exec){
+            $_SESSION['pesan'] = "Data supplier telah ditambahkan";
+            header('location:../../dashboard.php?modul=suplier');
+        }else{
+            $_SESSION['pesan'] = "Data supplier gagal ditambahkan";
+            header('location:../../dashboard.php?modul=suplier');
+        }
+    }elseif($_GET['act']=="update"){
+        $id_supplier = $_GET['id_supplier'];
+        $query = "UPDATE supplier SET nama_supplier='$nama_supplier',alamat='$alamat',telepon='$telepon' WHERE id_supplier='$id_supplier'";
+        $exec = mysqli_query($koneksi, $query);
+        if($exec){
+        $_SESSION['pesan'] = "Data supplier telah diubah";
+        header('location:../../dashboard.php?modul=suplier');
+        }else{
+        $_SESSION['pesan'] = "Data suuplier gagal diubah";
+        header('location:../../dashboard.php?modul=suplier');
+        }
+        echo "update";
+    }
+}
+else if($_SERVER['REQUEST_METHOD']=='GET'){
+    if($_GET['act']=="delete"){
+        $id_supplier = $_GET['id_supplier'];
+        $query = "DELETE FROM supplier WHERE id_supplier='$id_supplier'";
+        $exec = mysqli_query($koneksi, $query);
+        if($exec){
+            $_SESSION['pesan'] = "Data supplier telah dihapus";
+            header('location:../../dashboard.php?modul=suplier');
+        }else{
+            $_SESSION['pesan'] = "Data supplier gagal dihapus";
+            header('location:../../dashboard.php?modul=suplier');
+        }
+    }else{
+        header('location:../../index.php');
+    }
+}else{
+    echo 'Unknown';
+}
+
+?>
